fbcc1823709ad63a99dd63b022bbfd0123c16c60
Mayowa Daniel <mayowadan17@gmail.com>
2024-05-12 10:16:31 -0700

    finish smarter search feature

--- src/contentScript/components/DraggableButton/DraggableButton.tsx
+++ src/contentScript/components/DraggableButton/DraggableButton.tsx
@@ -13,6 +13,7 @@ import {
 import * as Separator from '@radix-ui/react-separator'
 
 const ORB_DIMENSIONS = 60
+
 const DraggableButton = () => {
   const dispatch = useAppDispatch()
 
@@ -60,29 +61,20 @@ const DraggableButton = () => {
               className="min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
               sideOffset={5}
             >
-              <DropdownMenu.Item
-                onSelect={() => {
-                  const allTextContent = document.body.innerText
-                  console.log(
-                    'All text content from the main website:',
-                    JSON.stringify(allTextContent),
-                  )
-                }}
-                className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-              >
-                Smarter Page Search
-                <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
-                  <MagnifyingGlassIcon />
-                </div>
-              </DropdownMenu.Item>
               <Dialog.Trigger asChild>
                 <DropdownMenu.Item className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
-                  Point and Ask Mode
+                  Smarter Page Search
                   <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
-                    <CursorArrowIcon />
+                    <MagnifyingGlassIcon />
                   </div>
                 </DropdownMenu.Item>
               </Dialog.Trigger>
+              <DropdownMenu.Item className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
+                Point and Ask Mode
+                <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
+                  <CursorArrowIcon />
+                </div>
+              </DropdownMenu.Item>
               <DropdownMenu.Item className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
                 Settings
                 <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
@@ -101,14 +93,7 @@ const DraggableButton = () => {
         <Dialog.Portal>
           <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
           <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-            {/* <Dialog.Close asChild>
-              <button
-                className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                aria-label="Close"
-              >
-                <Cross2Icon />
-              </button>
-            </Dialog.Close> */}
+            <DialogBody />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
@@ -117,24 +102,81 @@ const DraggableButton = () => {
 }
 
 const DialogBody = () => {
+  const [inputValue, setInputValue] = React.useState('')
+  const [apiResponse, setApiResponse] = React.useState('')
+  const [isLoading, setIsLoading] = React.useState(false) // Add this line
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleKeyDown = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      setIsLoading(true) // Set loading to true before the fetch call
+      const response = await fetch(
+        'https://88g84y9v17.execute-api.us-east-1.amazonaws.com/smarter-page-search',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ query: inputValue, siteText: document.body.innerText }),
+        },
+      )
+      const data = await response.json() // Assuming the response is plain text
+      setApiResponse(data.content)
+      setIsLoading(false) // Set loading to false after the fetch call
+    }
+  }
+
   return (
     <>
-      <fieldset className="mb-[15px] flex-col items-center gap-5">
+      <fieldset className="mb-[15px] flex items-center gap-x-4">
+        <div>{isLoading ? <LoadingSpinner /> : <MagnifyingGlassIcon />}</div>
         <input
           className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
           id="name"
-          defaultValue="Pedro Duarte"
+          placeholder="Ask me anything about this page"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
         />
       </fieldset>
-      <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-        Smarter Page Search
-      </Dialog.Title>
+
       <Dialog.Description className="text-mauve11 m-0 text-[13px]">
-        Smarter Page Search is a tool that uses AI to help you find the most relevant information on
-        a webpage. It's a simple way to get the most out of your data.
+        Smarter Page Search uses AI to help you find the most relevant information on a webpage.
       </Dialog.Description>
       <Separator.Root className="bg-violet6 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px my-[15px]" />
+      <p className="text-mauve11 m-0 text-[13px]">{apiResponse}</p>
     </>
+  )
+}
+
+const LoadingSpinner = () => {
+  return (
+    <div className="text-violet11">
+      <svg
+        className="animate-spin -ml-1 h-5 w-5 text-violet11"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
   )
 }
 
