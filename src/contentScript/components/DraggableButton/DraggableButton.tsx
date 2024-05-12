745edb9ebbae8145647fd9b6fff8e4a43f3cd279
Mayowa Daniel <mayowadan17@gmail.com>
2024-05-12 10:19:13 -0700

    bump icon size

--- src/contentScript/components/DraggableButton/DraggableButton.tsx
+++ src/contentScript/components/DraggableButton/DraggableButton.tsx
@@ -132,8 +132,8 @@ const DialogBody = () => {
 
   return (
     <>
-      <fieldset className="mb-[15px] flex items-center gap-x-4">
-        <div>{isLoading ? <LoadingSpinner /> : <MagnifyingGlassIcon />}</div>
+      <fieldset className="mb-[15px] flex items-center gap-5">
+        <div>{isLoading ? <LoadingSpinner /> : <MagnifyingGlassIcon className="h-5 w-5" />}</div>
         <input
           className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
           id="name"
