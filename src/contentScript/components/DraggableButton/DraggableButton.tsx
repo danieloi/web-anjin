9452f1e46fa30acb0c96b987ce898a441c32a277
Mayowa Daniel <mayowadan17@gmail.com>
2024-05-12 02:30:41 -0700

    get dropdown working with draggable button

--- src/contentScript/components/DraggableButton/DraggableButton.tsx
+++ src/contentScript/components/DraggableButton/DraggableButton.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
 import useDraggable from './useDraggable'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import { setActive } from '../../redux/slices/active.slice'
@@ -8,71 +8,73 @@ const DraggableButton = () => {
 
   const active = useAppSelector((state) => state.active.isActive)
 
+  const { buttonRef, position, handleMouseDown } = useDraggable()
   return (
-    <DropdownMenu.Root open={active} onOpenChange={(open) => dispatch(setActive(open))}>
-      {/* <DropdownMenu.Root open={active} onOpenChange={(open) => dispatch(setActive(open))}> */}
-      <DropdownMenu.Trigger asChild>
-        <Button />
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          className="min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-          sideOffset={5}
-        >
-          <DropdownMenu.Item className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
-            New Tab{' '}
-            <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
-              ⌘+T
-            </div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
-            New Window{' '}
-            <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
-              ⌘+N
-            </div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-            disabled
+    <>
+      <button
+        onDoubleClick={(e) => {
+          dispatch(setActive(!active))
+        }}
+        ref={buttonRef}
+        id="draggable-button"
+        className={active ? 'bg-red-400' : 'bg-blue-400'}
+        style={{
+          position: 'fixed',
+          left: `${position.current.x}px`,
+          top: `${position.current.y}px`,
+          zIndex: 1000,
+          borderRadius: '100%',
+          color: 'white',
+          width: '60px',
+          height: '60px',
+          borderColor: 'transparent',
+        }}
+        onMouseDown={handleMouseDown}
+      />
+      <DropdownMenu.Root open={active} onOpenChange={(open) => dispatch(setActive(open))}>
+        <DropdownMenu.Trigger asChild>
+          {/* hack to make dragging and triggers work */}
+          <div
+            style={{
+              position: 'fixed',
+              left: `${position.current.x}px`,
+              top: `${position.current.y}px`,
+              width: '60px',
+              height: '60px',
+            }}
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            className="z-50 min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+            sideOffset={5}
           >
-            New Private Window{' '}
-            <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
-              ⇧+⌘+N
-            </div>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+            <DropdownMenu.Item className="group text-[13px] leading-none text-violet-400 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-violet-200 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-900 data-[highlighted]:text-violet-100">
+              New Tab{' '}
+              <div className="ml-auto pl-[20px] text-violet-200 group-data-[highlighted]:text-white group-data-[disabled]:text-violet-200">
+                ⌘+T
+              </div>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className="group text-[13px] leading-none text-violet-400 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-violet-200 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-900 data-[highlighted]:text-violet-100">
+              New Window{' '}
+              <div className="ml-auto pl-[20px] text-violet-200 group-data-[highlighted]:text-white group-data-[disabled]:text-violet-200">
+                ⌘+N
+              </div>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item
+              className="group text-[13px] leading-none text-violet-400 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-violet-200 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-900 data-[highlighted]:text-violet-100"
+              disabled
+            >
+              New Private Window{' '}
+              <div className="ml-auto pl-[20px] text-violet-200 group-data-[highlighted]:text-white group-data-[disabled]:text-violet-200">
+                ⇧+⌘+N
+              </div>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+    </>
   )
 }
-
-const Button = forwardRef((props, ref: React.ForwardedRef<HTMLButtonElement>) => {
-  const active = useAppSelector((state) => state.active.isActive)
-
-  const { buttonRef, position, handleMouseDown } = useDraggable(ref)
-  return (
-    <button
-      {...props}
-      // onDoubleClick={(e) => {
-      //   dispatch(setActive(!active))
-      // }}
-      ref={buttonRef}
-      id="draggable-button"
-      className={active ? 'bg-red-400' : 'bg-blue-400'}
-      style={{
-        position: 'fixed',
-        left: `${position.current.x}px`,
-        top: `${position.current.y}px`,
-        zIndex: 1000,
-        borderRadius: '100%',
-        color: 'white',
-        width: '60px',
-        height: '60px',
-        borderColor: 'transparent',
-      }}
-      onMouseDown={handleMouseDown}
-    />
-  )
-})
 
 export default DraggableButton
--- src/contentScript/components/DraggableButton/useDraggable.ts
+++ src/contentScript/components/DraggableButton/useDraggable.ts
@@ -1,11 +1,13 @@
 import { useEffect, useRef } from 'react'
 
-const useDraggable = (buttonRef: React.RefObject<HTMLButtonElement>) => {
+// const useDraggable = (buttonRef: React.RefObject<HTMLButtonElement>) => {
+const useDraggable = () => {
   const isDragging = useRef(false)
   const position = useRef({ x: 20, y: 20 })
   const velocity = useRef({ x: 0, y: 0 })
   const lastPosition = useRef({ x: 0, y: 0 })
   const offset = useRef({ x: 0, y: 0 })
+  const buttonRef = useRef<HTMLButtonElement>(null)
 
   const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
     isDragging.current = true
