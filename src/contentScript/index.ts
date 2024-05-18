0deb291fa881817067db6c4e3246bfeec0ec2590
Mayowa Daniel <mayowadan17@gmail.com>
2024-05-18 14:10:40 -0700

    move extension ui to shadow dom

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,26 +1,36 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './index.css'
+import indexCss from './index.css?inline'
 
 const body = document.querySelector('body')
-const app = document.createElement('div')
+const appContainer = document.createElement('div')
 
-app.id = 'web-anjin-react-root'
+appContainer.id = 'web-anjin-react-root'
 
-if (body) {
-  body.appendChild(app)
-}
-const container = document.getElementById('web-anjin-react-root')
+try {
+  if (body) {
+    body.appendChild(appContainer)
+    const shadowRoot = appContainer.attachShadow({ mode: 'open' })
 
-if (container) {
-  const root = createRoot(container)
-  // Render react component with a 3 second delay
-  setTimeout(() => {
-    root.render(React.createElement(App, null))
-  }, 3000)
-} else {
+    // Create a style element for Tailwind CSS
+    const style = document.createElement('style')
+    // Assuming you have a build process that outputs Tailwind CSS to a specific file
+    // You need to fetch this CSS and then apply it here
+    console.log({ indexCss })
+    style.textContent = indexCss
+    shadowRoot.appendChild(style)
+
+    const reactRootDiv = document.createElement('div')
+    reactRootDiv.id = 'shadow-root-react-root'
+    shadowRoot.appendChild(reactRootDiv)
+
+    const root = createRoot(reactRootDiv)
+    setTimeout(() => {
+      root.render(React.createElement(App, null))
+    }, 3000)
+  }
+} catch (error) {
   console.error('Failed to find #react-root element')
 }
-
 console.info('contentScript is running')
